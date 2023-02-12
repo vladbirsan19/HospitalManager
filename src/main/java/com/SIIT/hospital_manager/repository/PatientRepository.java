78d0a45c147a5fd4b882ceb2127f1421d50b3302
George Agachi <georgeagache@ymail.com>
2023-02-12 05:45:29 +0200

    Added hide for Specialisation, Doctor and Patient

--- src/main/java/com/SIIT/hospital_manager/repository/PatientRepository.java
+++ src/main/java/com/SIIT/hospital_manager/repository/PatientRepository.java
@@ -5,10 +5,13 @@ import com.siit.hospital_manager.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
     Optional<Patient> findByUserName(String userName);
+
+    List<Patient> findAllByIsActive(boolean isActive);
 }
